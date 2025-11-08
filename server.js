6b5620c13da735eb0e833cd7ec4b5f8f7d50d548
Parth Dhankecha <pmd3290@gmail.com>
2025-11-08 17:51:45 +0530

    Change of chitic display add

--- server.js
+++ server.js
@@ -9,12 +9,32 @@ const moment = require("moment");
 // ====== CONFIG ======
 const LOOM_IP = process.env.LOOM_IP || "192.168.205.2";
 const LOOM_PORT = parseInt(process.env.LOOM_PORT || "502", 10);
-const UNIT_ID = parseInt(process.env.UNIT_ID || "85", 10); // try 1 or 85
+var UNIT_ID = parseInt(process.env.UNIT_ID || "85", 10); // try 1 or 85
 const START_ADDR = parseInt(process.env.START_ADDR || "5000", 10);
-const COUNT = parseInt(process.env.COUNT || "74", 10);
+var COUNT = parseInt(process.env.COUNT || "74", 10);
 const ZERO_BASED = true;
 const workspaceId = "68de43d477ac61e06d4c9f9f";
 var machineData = {};
+var REGISTER = {
+    nazon: {
+        stop: 5027,
+        shift: 5012,
+        setPicks: 5035,
+        clothLength: 5018,
+        loomState: 5028,
+        speed: 5010,
+        efficiency: 5017
+    },
+    chitic: {
+        stop: 5023,
+        shift: 5005,
+        setPicks: 5002,
+        clothLength: 5006,
+        loomState: 5013,
+        speed: 5003,
+        efficiency: 5044
+    }
+}
 
 // ====== HELPERS ======
 const sleep = (ms) => new Promise(r => setTimeout(r, ms));
@@ -35,6 +55,7 @@ async function pollLoop(machine) {
         connecting = true;
         try {
             await client.connectTCP(machine.ip, { port: LOOM_PORT });
+            machine.displayType == 'chitic' ? UNIT_ID = 1 : UNIT_ID = 85;
             client.setID(UNIT_ID);
             client.setTimeout(1000);
             lastError = null;
@@ -52,6 +73,7 @@ async function pollLoop(machine) {
 
     function initMachineData(machineId) {
         machineData[machineId] = {
+            displayType: machine.displayType,
             stopCount: 0,
             stopsData: {
                 warp: [],
@@ -75,67 +97,212 @@ async function pollLoop(machine) {
                 machineData[machineId].stopCount += 1;
             }
         }
-        switch (machineData[machineId].stop) {
-            case 1:
-            case 19:
-            case 20:
-                machineData[machineId].stopsData.warp.push({
-                    start: machineData[machineId].lastStopTime,
-                    end: moment().utc().format(),
-                    statusCode: machineData[machineId].stop,
-                    duration: stopDuration
-                });
-                break;
-
-            case 2: 
-            case 3:
-            case 11:
-            case 12:
-            case 15:
-            case 16:
-            case 17:
-            case 18:
-                machineData[machineId].stopsData.weft.push({
-                    start: machineData[machineId].lastStopTime,
-                    end: moment().utc().format(),
-                    statusCode: machineData[machineId].stop,
-                    duration: stopDuration
-                });
-                break;
-
-            case 7:
-                machineData[machineId].stopsData.feeder.push({
-                    start: machineData[machineId].lastStopTime,
-                    end: moment().utc().format(),
-                    statusCode: machineData[machineId].stop,
-                    duration: stopDuration
-                });
-                break;
-
-            case 4:
-            case 6:
-                machineData[machineId].stopsData.manual.push({
-                    start: machineData[machineId].lastStopTime,
-                    end: moment().utc().format(),
-                    statusCode: machineData[machineId].stop,
-                    duration: stopDuration
-                });
-                break;
-
-            default:
-                machineData[machineId].stopsData.other.push({
-                    start: machineData[machineId].lastStopTime,
-                    end: moment().utc().format(),
-                    duration: stopDuration,
-                    statusCode: machineData[machineId].stop
-                })
-                break;
+        if(machine.displayType == 'nazon') {
+            switch (machineData[machineId].stop) {
+                case 1:
+                case 19:
+                case 20:
+                    machineData[machineId].stopsData.warp.push({
+                        start: machineData[machineId].lastStopTime,
+                        end: moment().utc().format(),
+                        statusCode: machineData[machineId].stop,
+                        duration: stopDuration
+                    });
+                    break;
+    
+                case 2: 
+                case 3:
+                case 11:
+                case 12:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                    machineData[machineId].stopsData.weft.push({
+                        start: machineData[machineId].lastStopTime,
+                        end: moment().utc().format(),
+                        statusCode: machineData[machineId].stop,
+                        duration: stopDuration
+                    });
+                    break;
+    
+                case 7:
+                    machineData[machineId].stopsData.feeder.push({
+                        start: machineData[machineId].lastStopTime,
+                        end: moment().utc().format(),
+                        statusCode: machineData[machineId].stop,
+                        duration: stopDuration
+                    });
+                    break;
+    
+                case 4:
+                case 6:
+                    machineData[machineId].stopsData.manual.push({
+                        start: machineData[machineId].lastStopTime,
+                        end: moment().utc().format(),
+                        statusCode: machineData[machineId].stop,
+                        duration: stopDuration
+                    });
+                    break;
+    
+                default:
+                    machineData[machineId].stopsData.other.push({
+                        start: machineData[machineId].lastStopTime,
+                        end: moment().utc().format(),
+                        duration: stopDuration,
+                        statusCode: machineData[machineId].stop
+                    })
+                    break;
+            }
+        } else if(machine.displayType == 'chitic') {
+            switch (machineData[machineId].stop) {
+                case 1:
+                    machineData[machineId].stopsData.warp.push({
+                        start: machineData[machineId].lastStopTime,
+                        end: moment().utc().format(),
+                        statusCode: machineData[machineId].stop,
+                        duration: stopDuration
+                    });
+                    break;
+    
+                case 2: 
+                case 3:
+                case 11:
+                case 12:
+                    machineData[machineId].stopsData.weft.push({
+                        start: machineData[machineId].lastStopTime,
+                        end: moment().utc().format(),
+                        statusCode: machineData[machineId].stop,
+                        duration: stopDuration
+                    });
+                    break;
+    
+                case 7:
+                    machineData[machineId].stopsData.feeder.push({
+                        start: machineData[machineId].lastStopTime,
+                        end: moment().utc().format(),
+                        statusCode: machineData[machineId].stop,
+                        duration: stopDuration
+                    });
+                    break;
+    
+                case 4:
+                case 6:
+                    machineData[machineId].stopsData.manual.push({
+                        start: machineData[machineId].lastStopTime,
+                        end: moment().utc().format(),
+                        statusCode: machineData[machineId].stop,
+                        duration: stopDuration
+                    });
+                    break;
+    
+                default:
+                    machineData[machineId].stopsData.other.push({
+                        start: machineData[machineId].lastStopTime,
+                        end: moment().utc().format(),
+                        duration: stopDuration,
+                        statusCode: machineData[machineId].stop
+                    })
+                    break;
+            }
         }
     }
 
-    function processData(machineId, deviceType, data) {
+    function parseBlock(body, displayType = 'chitic') {
+        const toUint32 = (hi, lo) => (((hi << 16) >>> 0) + (lo >>> 0)) >>> 0;
+        const at = (lw) => body[lw - 4999];
+
+        const speedRpm   = displayType == "nazon" ? at(5010) : at(5003);
+        const stopCode   = displayType == "nazon" ? at(5027) : at(5023);
+        const stateCode  = displayType == "nazon" ? at(5028) : at(5013);
+        const efficiency = displayType == "nazon" ? at(5017) : at(5044);
+
+        const pieceLenCm     = displayType == "nazon" ? at(5013) : at(5045)/10;
+        const pieceLenMeters = parseFloat((pieceLenCm / 100).toFixed(2));
+
+        const shiftWeftCount = displayType == "nazon" ? toUint32(at(5016), at(5015)) : toUint32(at(5048), at(5047));
+        const totalWeftHundreds = displayType == "nazon" ? toUint32(at(5020), at(5019)) : 0;
+        const totalWeftCount = totalWeftHundreds * 100;
+        const currentDensity = displayType == "nazon" ? at(5035) : at(5002);
+
+        const beamLeft = displayType == "nazon" ? at(5023) : at(5022);
+
+        const alarms = displayType == "nazon" ? [at(5029), at(5030), at(5031), at(5032)] : [];
+        let stopsCount = {};
+        if(displayType == "nazon"){
+            stopsCount = {
+                warp: {
+                    count: at(5061),
+                    duration: at(5057) || 0
+                },
+                weft: {
+                    count: at(5062),
+                    duration: at(5058) || 0
+                },
+                feeder: {
+                    count: at(5063),
+                    duration: at(5059) || 0
+                },
+                manual: {
+                    count: 0,
+                    duration: 0
+                },
+                other: {
+                    count: at(5064),
+                    duration: at(5060) || 0
+                }
+            }
+        } else if(displayType == "chitic") {
+            stopsCount = {
+                warp: {
+                    count: at(5036),
+                    duration: (at(5040) || 0) * 1 
+                },
+                weft: {
+                    count: at(5037),
+                    duration: (at(5041) || 0) * 1
+                },
+                manual: {
+                    count: at(5038),
+                    duration: (at(5042) || 0) * 1
+                },
+                feeder: {
+                    count: at(5049),
+                    duration: (at(5050) || 0) * 1
+                },
+                other: {
+                    count: at(5039),
+                    duration: (at(5043) || 0) * 1
+                }
+            }
+        }
+
+        console.log({
+            speedRpm: speedRpm,
+            efficiencyPercent: efficiency,
+            stop: stopCode,
+            loomStateCode: stateCode,
+            picksCurrentShift: shiftWeftCount,
+            picksTotal: totalWeftCount,
+            pieceLengthM: pieceLenMeters,
+            beamLeft: beamLeft,
+            setPicks: currentDensity,
+            alarmsActive: alarms,
+            shift: at(5012),
+            stopsCount: stopsCount,
+            runTime: (displayType == "nazon" ? `${at(5055).toString().padStart(2, '0')}:${at(5056).toString().padStart(2, '0')}` : `${at(5034).toString().padStart(2, '0')}:${at(5035).toString().padStart(2, '0')}`)
+        });
+    }
+
+    function processData(machineId, deviceType, displayType="nazon", data) {
         const at = (lw) => data[lw - 4999];
-        const stop = at(5027);
+        let speed = at(REGISTER[machine.displayType].speed);
+        let stop = at(REGISTER[machine.displayType].stop);
+        if(machine.displayType == "chitic"){
+            if(speed > 5) {
+                data[REGISTER[machine.displayType].stop - 4999] = 0;
+            }
+        }
         if ((!machineData[machineId] && stop != 0) || (machineData[machineId] && machineData[machineId].stop == 0 && stop != 0)) {
             if (!machineData[machineId]) initMachineData(machineId);
             machineData[machineId].lastStopTime = moment().utc().format();
@@ -146,7 +313,7 @@ async function pollLoop(machine) {
             machineData[machineId].lastStartTime = moment().utc().format();
             setStopData(machineId, data);
         }
-        if(typeof machineData[machineId].shift == "number" && at(5012) != machineData[machineId].shift) {
+        if(typeof machineData[machineId].shift == "number" && at(REGISTER[machine.displayType].shift) != machineData[machineId].shift) {
             if(machineData[machineId].stop != 0 && stop != 0) {
                 setStopData(machineId, data);
                 machineData[machineId].lastStopTime = moment().utc().format();
@@ -165,11 +332,14 @@ async function pollLoop(machine) {
             }
         }
         machineData[machineId].stop = stop;
-        if(deviceType == 'rs485') {
-            data[36] = data[36] / 10;
+        if(deviceType == 'rs485' || displayType == 'chitic') {
+            data[REGISTER[machine.displayType].setPicks - 4999] = at(REGISTER[machine.displayType].setPicks) / 10;
+        }
+        if(displayType == 'chitic') {
+            data[REGISTER[machine.displayType].efficiency - 4999] = at(REGISTER[machine.displayType].efficiency);
         }
         machineData[machineId].rawData = data;
-        machineData[machineId].shift = at(5012);
+        machineData[machineId].shift = at(REGISTER[machine.displayType].shift);
     }
     for (; ;) {
         try {
@@ -179,11 +349,11 @@ async function pollLoop(machine) {
             if (client.isOpen) {
                 let start = ZERO_BASED ? (START_ADDR - 1) : START_ADDR;
                 const resp = await client.readHoldingRegisters(start, COUNT);
-                
-                if(resp.data && resp.data.length > 30 && resp.data[19] == 0 && resp.data[29] == 0 && resp.data[11] == 0) {
+
+                if(resp.data && resp.data.length > 30 && resp.data[REGISTER[machine.displayType].clothLength - start] == 0 && resp.data[REGISTER[machine.displayType].loomState - start] == 0 && resp.data[REGISTER[machine.displayType].speed - start] == 0) {
                     console.log(resp.data);
                 } else {
-                    processData(machine.id, machine.deviceType, resp.data);
+                    processData(machine.id, machine.deviceType, machine.displayType, resp.data);
                 }
                 lastError = null;
                 backoffMs = 1000; // reset backoff on success
@@ -234,7 +404,7 @@ setInterval(async () => {
         console.log(error);
         isDataStorAPICalled = false;
     }
-}, 4000);
+}, 5000);
 
 const PORT = parseInt(process.env.PORT || "3001", 10);
 app.listen(PORT, () => {
